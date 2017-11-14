098fd575fb2908b25718717181891afd0ca8c228
jGold6001 <jgold6001@gmail.com>
2017-11-14 13:14:59 +0200

    AssignCategory making)

---
+++ Multilevel architecture/NLayerApp/NLayerApp.BLL/Interfaces/IProductService.cs
@@ -0,0 +1,18 @@
+﻿using NLayerApp.BLL.DTO;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace NLayerApp.BLL.Interfaces
+{
+    public interface IProductService
+    {
+        void CreateProduct(ProductDTO productDTO);
+        void AssignCategory(ProductDTO productDTO, CategoryDTO categoryDTO);
+        ProductDTO GetProduct(int? id);
+        IEnumerable<ProductDTO> GetProducts();
+        void Dispose();
+    }
+}
---
+++ Multilevel architecture/NLayerApp/NLayerApp.BLL/Services/ProductService.cs
@@ -0,0 +1,71 @@
+﻿using NLayerApp.BLL.Interfaces;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using NLayerApp.BLL.DTO;
+using NLayerApp.DAL.Interfaces;
+using AutoMapper;
+using NLayerApp.DAL.Entities;
+using NLayerApp.BLL.Infrastructure;
+using NLayerApp.BLL.BusinessModels;
+
+namespace NLayerApp.BLL.Services
+{
+    public class ProductService : IProductService
+    {
+        IUnitOfWork Database { get; set; }
+
+        public ProductService(IUnitOfWork database)
+        {
+            this.Database = database;
+        }
+
+        public void CreateProduct(ProductDTO productDTO)
+        {
+            //assign discount
+            productDTO.Price = new Discount(0.1m).GetDiscountedPrice(productDTO.Price);
+
+            Mapper.Initialize(cfg => cfg.CreateMap<ProductDTO, Product>());
+            Product product = Mapper.Map<ProductDTO, Product>(productDTO);
+
+            Database.Products.AddOrUpdate(product);
+            Database.Save();
+        }
+
+        public void Dispose()
+        {
+            Database.Dispose();
+        }
+
+        public ProductDTO GetProduct(int? id)
+        {
+            if (id == null)
+                throw new ValidationException("Not set 'id' of product", "");
+
+            var product = Database.Products.Get(id.Value);
+            if(product == null)
+                throw new ValidationException("Product not found", "");
+
+            Mapper.Initialize(cfg => cfg.CreateMap<Product, ProductDTO>());
+            return Mapper.Map<Product, ProductDTO>(product); ;
+        }
+
+        public IEnumerable<ProductDTO> GetProducts()
+        {
+            Mapper.Initialize(cfg => cfg.CreateMap<Product, ProductDTO>());
+            return Mapper.Map<IEnumerable<Product>, List<ProductDTO>>(Database.Products.GetAll()); ;
+        }
+
+        public void AssignCategory(ProductDTO productDTO, CategoryDTO categoryDTO)
+        {
+            Product product = Database.Products.Get(productDTO.Id);
+            if (product == null)
+                throw new ValidationException("Product not found", "");
+
+            //GOTO
+
+        }
+    }
+}
