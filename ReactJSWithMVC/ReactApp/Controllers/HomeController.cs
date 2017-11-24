edb462f442c1785d458ce70fe009e05f422094e7
jGold6001 <jgold6001@gmail.com>
2017-11-24 23:36:32 +0200

    add ReactJS project

---
+++ ReactJSWithMVC/ReactApp/Controllers/HomeController.cs
@@ -0,0 +1,46 @@
+﻿using ReactApp.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace ReactApp.Controllers
+{
+    public class HomeController : Controller
+    {
+        static List<Phone> data = new List<Phone>
+        {
+            new Phone { Id = Guid.NewGuid().ToString(), Name="iPhone 7", Price=52000 },
+            new Phone { Id = Guid.NewGuid().ToString(), Name="Samsung Galaxy S7", Price=42000 },
+        };
+        public ActionResult Index()
+        {
+            return View();
+        }
+        public ActionResult GetPhones()
+        {
+            return Json(data, JsonRequestBehavior.AllowGet);
+        }
+
+        [HttpPost]
+        public ActionResult AddPhone(Phone phone)
+        {
+            phone.Id = Guid.NewGuid().ToString();
+            data.Add(phone);
+            return Json(phone);
+        }
+
+        [HttpDelete]
+        public ActionResult DeletePhone(string id)
+        {
+            Phone phone = data.FirstOrDefault(x => x.Id == id);
+            if (phone != null)
+            {
+                data.Remove(phone);
+                return Json(phone);
+            }
+            return HttpNotFound();
+        }
+    }
+}
