bfb3f9b3daac7d2cfc868c81733cb8bfd943a564
jGold6001 <jgold6001@gmail.com>
2017-12-03 23:10:14 +0200

    Chat Room is ready)

---
+++ SignalR2/SignalRMvc/Hubs/ChatHub.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using Microsoft.AspNet.SignalR;
+using SignalRMvc.Models;
+using System.Threading.Tasks;
+
+namespace SignalRMvc.Hubs
+{
+    public class ChatHub : Hub
+    {
+        static List<User> Users = new List<User>();
+
+        // Sending messages
+        public void Send(string name, string message)
+        {
+            Clients.All.addMessage(name, message);
+        }
+
+        // Connect a new user
+        public void Connect(string userName)
+        {
+            var id = Context.ConnectionId;
+
+
+            if (!Users.Any(x => x.ConnectionId == id))
+            {
+                Users.Add(new User { ConnectionId = id, Name = userName });
+
+                // Send the message to the current user
+                Clients.Caller.onConnected(id, userName, Users);
+
+                // We send a message to all users except the current one
+                Clients.AllExcept(id).onNewUserConnected(id, userName);
+            }
+        }
+
+        // Disconnect the user
+        public override Task OnDisconnected(bool stopCalled)
+        {
+            var item = Users.FirstOrDefault(x => x.ConnectionId == Context.ConnectionId);
+            if (item != null)
+            {
+                Users.Remove(item);
+                var id = Context.ConnectionId;
+                Clients.All.onUserDisconnected(id, item.Name);
+            }
+
+            return base.OnDisconnected(stopCalled);
+        }
+    }
+}
---
+++ SignalR2/SignalRMvc/Scripts/util.js
@@ -0,0 +1,85 @@
+﻿$(function () {
+
+    $('#chatBody').hide();
+    $('#loginBlock').show();
+    //Reference to the auto- generated hub proxy
+    var chat = $.connection.chatHub;
+
+    // Declare the function that the hub calls when it receives messages
+    chat.client.addMessage = function (name, message) {
+
+        // Adding messages to a web page
+        $('#chatroom').append('<p><b>' + htmlEncode(name)
+            + '</b>: ' + htmlEncode(message) + '</p>');
+    };
+
+    // Function that is called when a new user is connected
+    chat.client.onConnected = function (id, userName, allUsers) {
+
+        $('#loginBlock').hide();
+        $('#chatBody').show();
+
+        // set the hidden name and id of the current user
+        $('#hdId').val(id);
+        $('#username').val(userName);
+        $('#header').html('<h3>Wellcome, ' + userName + '</h3>');
+
+        // Add all users
+        for (i = 0; i < allUsers.length; i++) {
+
+            AddUser(allUsers[i].ConnectionId, allUsers[i].Name);
+        }
+    }
+
+    // Add a new user
+    chat.client.onNewUserConnected = function (id, name) {
+
+        AddUser(id, name);
+    }
+
+    // Delete the user
+    chat.client.onUserDisconnected = function (id, userName) {
+
+        $('#' + id).remove();
+    }
+
+    // Open the connection
+    $.connection.hub.start().done(function () {
+
+        $('#sendmessage').click(function () {
+
+            // Call the Send method on the hub
+            chat.server.send($('#username').val(), $('#message').val());
+            $('#message').val('');
+        });
+
+        // handle login
+        $("#btnLogin").click(function () {
+
+            var name = $("#txtUserName").val();
+            if (name.length > 0) {
+                chat.server.connect(name);
+            }
+            else {
+                alert("Enter name");
+            }
+        });
+    });
+});
+
+// Encode tag
+function htmlEncode(value) {
+    var encodedValue = $('<div />').text(value).html();
+    return encodedValue;
+}
+
+// Add a new user
+function AddUser(id, name) {
+
+    var userId = $('#hdId').val();
+
+    if (userId != id) {
+
+        $("#chatusers").append('<p id="' + id + '"><b>' + name + '</b></p>');
+    }
+}
