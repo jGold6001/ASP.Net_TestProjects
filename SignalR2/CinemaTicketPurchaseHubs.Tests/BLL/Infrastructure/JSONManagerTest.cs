6f1778df6ce2f77c5c7c9db89f0c0e1c9c6b3f09
jGold6001 <jgold6001@gmail.com>
2017-12-05 23:53:34 +0200

    Work with TicketsView)

---
+++ SignalR2/CinemaTicketPurchaseHubs.Tests/BLL/Infrastructure/JSONManagerTest.cs
@@ -0,0 +1,22 @@
+﻿using CinemaTicketPurchaseHubs.BLL.Infrastructure;
+using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace CinemaTicketPurchaseHubs.BLL.Infrastructure.Tests
+{
+    [TestClass()]
+    public class JSONManagerTest
+    {
+        JSONManager jManager = new JSONManager(@"F:\ITstep\Subjects\Diplom\ASP.Net_TestProjects\SignalR2\CinemaTicketPurchaseHubs\Resources\Json\mpx_skymall.json");
+
+        [TestMethod()]
+        public void TestCoinvertToDictionary()
+        {
+            var data = jManager.CoinvertToDictionary();
+
+
+
+        }
+    }
+}
+
--- SignalR2/CinemaTicketPurchaseHubs/BLL/Infrastructure/JSONManager.cs
+++ SignalR2/CinemaTicketPurchaseHubs/BLL/Infrastructure/JSONManager.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using Newtonsoft.Json;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Web;
@@ -7,6 +8,15 @@ namespace CinemaTicketPurchaseHubs.BLL.Infrastructure
 {
     public class JSONManager
     {
+        public string Path { get; private set; }
+        public JSONManager(string path)
+        {
+            Path = path;
+        }
 
+        public Dictionary<int, int> CoinvertToDictionary()
+        {
+            return JsonConvert.DeserializeObject<Dictionary<int, int>>(Path);
+        }
     }
 }
